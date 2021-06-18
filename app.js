8c76932b88ed5f7087cbc6e5cbb7c9b9aa7d0962
trung <vanhuuthanhtrung@gmail.com>
2021-06-18 13:31:07 +0300

    add project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+
+// Static assets
+app.use(express.static("./methods-public"));
+// Parse form data
+app.use(express.urlencoded({ extended: false }));
+// Parse json
+app.use(express.json());
+
+app.use('/api/people',people)
+
+app.use('/login',auth)
+
+app.listen(5000, () => {
+    console.log("server is listening on port 5000");
+});
