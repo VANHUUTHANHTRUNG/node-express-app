9af9d1dc6bc5171bd27cfdb1e02f960f06d6e5cc
trung <vanhuuthanhtrung@gmail.com>
2021-07-02 13:41:21 +0300

    update dynamic port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const people = require('./routes/people')
 const auth = require('./routes/auth')
+const port = process.env.PORT || 5000
 
 // Static assets
 app.use(express.static("./methods-public"));
@@ -15,6 +16,6 @@ app.use('/api/people',people)
 
 app.use('/login',auth)
 
-app.listen(5000, () => {
-    console.log("server is listening on port 5000");
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
 });
